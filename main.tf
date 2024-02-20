b81a57817d79d147688b651331cc084e9c6a4f4c
thierryjez <159033765+thierryjez@users.noreply.github.com>
2024-02-20 14:16:14 +0100

    create security group

--- main.tf
+++ main.tf
@@ -45,6 +45,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   security_group_id = aws_security_group.blog.id
 }
 
+
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
@@ -62,27 +63,4 @@ resource "aws_security_group_rule" "blog_everything_out" {
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
-}data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
